98ba8069ee0e1263c84c1e7458286195bde363a5
Johnson Fu <johnson.fu@gmail.com>
2019-11-15 22:15:08 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      meaningOfLife:47
+    }
+  }
+
+  // the set state is asynchronize
+  handleClick = (e) => {
+    // let count = this.state.meaningOfLife;
+    // this.setState({meaningOfLife: count + 1});
+    this.setState((prevState, prevProps) => {
+      return {meaningOfLife: prevState.meaningOfLife + prevProps.increment}
+    },
+      () => {console.log(this.state.meaningOfLife)}
+    )
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          <p>
+            {this.state.meaningOfLife}
+          </p>
+          <button onClick={this.handleClick}>
+            update
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
